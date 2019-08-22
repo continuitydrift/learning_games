3785b24a68ca4c0e72c66b5617d72957516f86d0
Chad Hines <chadahines@gmail.com>
2019-08-22 11:46:36 -0500

    Improving preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -1,5 +1,8 @@
-#Question Game
+#Preguntas
 #Simpler, Spanish Version
+
+## To Do
+### keep track of score
 
 
 ## Steps
@@ -10,21 +13,24 @@
 
 import os, random, time, json
 
-
+score=0
+high_score=0
 
 def load_vocab():
 	global vocab
 	if os.path.isfile("vocab.json"):
-		print ("vocab file exists")
+		#print ("vocab file exists")
 		try:
 			with open("vocab.json", "r") as readfile:
 				vocab = json.load(readfile)
 		except:
-			("couldn't load vocab from file.")
+			print ("couldn't load vocab from file.")
+	else:
+		print ("vocab.json doesn't exist.")
 	
 
 def ask_question():
-#	global vocab
+	global score
 #	for i in vocab:
 #		print(i)
 #	for key,val in vocab.items():
@@ -32,15 +38,33 @@ def ask_question():
 	word=random.choice(list(vocab.keys()))
 
 	print (word)
-	ans=input("what does that word mean in English?")
+	ans=input("what does that mean in English?")
 
 	print ("The right answer is", vocab[word])
 	response=input("were you close? y/n")
 	if response==("y"):
+		score+=1
+		print ("your score is", score)
 		ask_question()
-	
+	else:
+		end_game()
+
+def end_game():
+	global high_score, score
+	print ("your score was", score)
+	if score>high_score:
+		print ("You have acheived the high score!")
+	ans=input("Do you want to play again? y/n")
+	if ans=="yes" or ans=="y":
+		score=0
+		ask_question()
+
+def turn():
+#	global score
+	ask_question()	
 
 if __name__ == '__main__':
     load_vocab()
-    ask_question()
+    turn()
+   
 
